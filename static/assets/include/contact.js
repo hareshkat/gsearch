3b69121f27d24edd182f2dce9cfdbc2ea92bc51c
haresh.k <haresh.k@pepperfry.com>
2023-03-26 21:10:59 +0530

    gsearch

---
+++ static/assets/include/contact.js
@@ -0,0 +1,27 @@
+var contact = '' +
+' \
+  <section class="p-contact contact-main">\
+  <div class="contact__inner l-inner">\
+    <h2 class="contact__ttl sec-ttl">\
+      <span>Contact</span>\
+      <b>お問い合わせ</b>\
+    </h2>\
+    <p class="contact__lead sec-lead">お問い合わせフォームまたはお電話からお気軽にどうぞ。</p>\
+    <div class="contact-wrap">\
+      <p class="contact__btn">\
+        <a class="c-btn-01 c-btn-01--white" href="/inquiry/">\
+          <i><svg><use xlink:href="#ico-mail"></use></svg></i>\
+          <span class="c-btn-01__txt">お問い合わせフォーム</span>\
+        </a>\
+        <a class="c-btn-01 c-btn-01--white btn-tel" href="tel:03-5610-7755">\
+          <span class="c-btn-01__txt">お電話からのお問い合わせ</span>\
+          <b class="c-btn-01__tel">03-5610-7755</b>\
+          <span class="c-btn-01__txt">月～金（祝日・年末年始を除く）</span>\
+        </a>\
+      </p>\
+    </div>\
+  </div>\
+  </section>\
+  <!-- /.contact -->\
+';
+document.write(contact);
