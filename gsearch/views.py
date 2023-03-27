c819f842c463539fc738aa799fb4fa01b19f62b4
haresh.k <haresh.k@pepperfry.com>
2023-03-27 09:39:41 +0530

    view changes

--- gsearch/views.py
+++ gsearch/views.py
@@ -50,7 +50,7 @@ def download_file(request):
     if 'scraped_data' in request.session:
         context = request.session['scraped_data']
         response = HttpResponse(content_type='text/csv')
-        filename = "GoogleScrap.xlsx"
+        filename = "GoogleScrap.xls"
         response['Content-Disposition'] = f'attachment; filename="{filename}"'
         writer = csv.writer(response)
         writer.writerow(['Title', 'Link'])
